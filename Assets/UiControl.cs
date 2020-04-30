91525715fa1b38188927dd7305fc402ba437a408
Logan Brewer <logan@fbstudios.com>
2020-04-30 08:19:49 -0500

    Initial Push w/ assets

---
+++ Assets/UiControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiControl : MonoBehaviour {
+
+    public GameObject darken;
+    public GameObject menu;
+
+    public void OnMenuClicked() {
+        bool state = (menu.activeSelf);
+        menu.SetActive(!state);
+        darken.SetActive(!state);
+    }
+
+}
